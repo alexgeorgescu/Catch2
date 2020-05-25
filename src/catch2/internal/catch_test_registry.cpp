cbb9ca69e8fc1f61a678f1acb34d96d7d73b6e38
Martin Hořeňovský <martin.horenovsky@gmail.com>
2020-05-25 09:45:24 +0200

    WIP: replace std::unique_ptr with Catch::Detail::unique_ptr

--- src/catch2/internal/catch_test_registry.cpp
+++ src/catch2/internal/catch_test_registry.cpp
@@ -11,21 +11,24 @@
 #include <catch2/internal/catch_test_case_registry_impl.hpp>
 #include <catch2/interfaces/catch_interfaces_registry_hub.hpp>
 
+#include <memory>
+
 namespace Catch {
 
-    std::unique_ptr<ITestInvoker> makeTestInvoker( void(*testAsFunction)() ) {
-        return std::make_unique<TestInvokerAsFunction>( testAsFunction );
+    Detail::unique_ptr<ITestInvoker> makeTestInvoker( void(*testAsFunction)() ) {
+        return Detail::unique_ptr<ITestInvoker>( new TestInvokerAsFunction( testAsFunction ));
     }
 
-    AutoReg::AutoReg( std::unique_ptr<ITestInvoker> invoker, SourceLineInfo const& lineInfo, StringRef const& classOrMethod, NameAndTags const& nameAndTags ) noexcept {
+    AutoReg::AutoReg( Detail::unique_ptr<ITestInvoker> invoker, SourceLineInfo const& lineInfo, StringRef const& classOrMethod, NameAndTags const& nameAndTags ) noexcept {
         CATCH_TRY {
             getMutableRegistryHub()
                     .registerTest(
-                        makeTestCaseInfo(
+                        std::unique_ptr<TestCaseInfo>(makeTestCaseInfo(
                             extractClassName( classOrMethod ),
                             nameAndTags,
-                            lineInfo),
-                        std::move(invoker));
+                            lineInfo).release()),
+                        std::unique_ptr<ITestInvoker>(invoker.release())
+                    );
         } CATCH_CATCH_ALL {
             // Do not throw when constructing global objects, instead register the exception to be processed later
             getMutableRegistryHub().registerStartupException();
